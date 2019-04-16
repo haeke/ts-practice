1a9995137ad338105cfb5d4b433e4ed96e66eeff
Edwin Rivera <waehake@gmail.com>
2019-04-16 08:53:25 -0400

    added Object Types that have optional properties.

--- contactinfo/contactinfo.ts
+++ contactinfo/contactinfo.ts
@@ -18,3 +18,31 @@ let contactInfo: HasEmail | HasPhoneNumber =
     : { name: "Ed", phone: 1234567890 };
 // contactInfo can only access the .name property.
 contactInfo.name;
+
+// Create a union that will let you define an object that includes all of the properties of the HasEmail and HasPhoneNumber Interfaces. Note - this object must contain all the properties of the object.
+let combineContactInfo: HasEmail & HasPhoneNumber = {
+  name: "Ed",
+  email: "ed@gmailsnailmail.com",
+  phone: 1234567890
+};
+
+// Adding a ? mark to an Object Type makes the property optional.
+interface LuxuryCar {
+  manufacturer: string;
+  leatherInterior?: boolean;
+  selfDriving?: boolean;
+  allWheelDrive?: boolean;
+}
+// Create an object that does not contain the selfDriving property.
+let ATS: LuxuryCar = {
+  manufacturer: "Cadillac",
+  leatherInterior: true,
+  allWheelDrive: false
+};
+
+let E350: LuxuryCar = {
+  manufacturer: "Mercedes-Benz",
+  leatherInterior: true,
+  selfDriving: false,
+  allWheelDrive: false
+};
