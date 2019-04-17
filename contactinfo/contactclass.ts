40882757efbb409c6492d6be045d1343396cea19
Edwin Rivera <waehake@gmail.com>
2019-04-17 10:21:20 -0400

    Added an example of an obstract class, that uses an abstract field and abstract function.

--- contactinfo/contactclass.ts
+++ contactinfo/contactclass.ts
@@ -10,8 +10,14 @@ class ContactPhone implements HasPhoneNumber {
     this.phone = phone;
   }
 }
-
 let me = new ContactPhone("edwin", 5552255);
+
+// ParamProperties allow for a less verbose syntax when defining a class by defining an ACCESS MODIFIER KEYWORD: public, private, or protected to the arguments inside the constructor function.
+class ParamPropContactPhone implements HasPhoneNumber {
+  constructor(public name: string, public phone: number) {}
+}
+
+let paramme = new ParamPropContactPhone("edwin", 12345);
 
 class ContactEmail implements HasEmail {
   // the name and email fields need to be declared so that the class knows the fields exist.
@@ -26,3 +32,32 @@ class ContactEmail implements HasEmail {
 let you = new ContactEmail("ed", "myemail@gmail.com");
 
 // The takeaway here is that "implements" defines a class that aligns with an interface.
+
+// An example of a class that uses implements on multiple interfaces and has an protected value that is initialized to 0.
+
+class OtherContact implements HasPhoneNumber, HasEmail {
+  protected age: number = 0;
+  constructor(
+    public name: string,
+    public email: string = "no email",
+    public phone: number
+  ) {}
+  // Create a public function that will return the age of the OtherContact
+  public getAge(): number {
+    return this.age;
+  }
+}
+
+let otherContact = new OtherContact("Edwin", "example@gmail.com", 12345);
+let newContact = new OtherContact("edwin", undefined, 12345);
+console.log(newContact.email);
+console.log(otherContact.getAge());
+
+// TypeScript Allows for Abstract classes - this type of class cannot be instantiated on its own. In addition Abstract functions, where the function will need to be called when using this class.
+abstract class AbstractContact implements HasPhoneNumber, HasEmail {
+  // must be implemented by a non-abstract class.
+  public abstract email: string;
+  constructor(public name: string, public phone: number) {}
+  // The sendEmail function needs to be implemented by a non-abstract class.
+  abstract sendEmail(): void;
+}
