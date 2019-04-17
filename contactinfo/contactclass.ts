8ef9145d807ca863b1a7c419f923468dea3857fa
Edwin Rivera <waehake@gmail.com>
2019-04-17 09:37:35 -0400

    Added a contactclass file that defines two classes that implements the HasPhoneNumber and HasEmail interfaces.

---
+++ contactinfo/contactclass.ts
@@ -0,0 +1,26 @@
+import { HasPhoneNumber, HasEmail } from "./contactinfo";
+
+class ContactPhone implements HasPhoneNumber {
+  // the name and phone needs to be declared so that the class knows that these member fields exist.
+  name: string;
+  phone: number;
+  // same rule when defining arguments, the types are required. constructor functions do not contain return types.
+  constructor(name: string, phone: number) {
+    this.name = name;
+    this.phone = phone;
+  }
+}
+
+let me = new ContactPhone("edwin", 5552255);
+
+class ContactEmail implements HasEmail {
+  // the name and email fields need to be declared so that the class knows the fields exist.
+  name: string;
+  email: string;
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
+let you = new ContactEmail("ed", "myemail@gmail.com");
--- generalpractice/typesandinterfaces.ts
+++ generalpractice/typesandinterfaces.ts
@@ -1,11 +1,14 @@
 // An example of a type that uses an interface
 // The NumVal type can be a 1, 2, 3 or an array of 1's,2's and 3's.
 type NumVal = 1 | 2 | 3 | NumArr;
-// Create an interface type that extends the NumVal type of type Array
+// Create an interface type that extends the NumVal type of type Array - This is an array of objects of the NumVal type.
 interface NumArr extends Array<NumVal> {}
 // Tests
 let testPrimitiveOne: NumVal = 1;
 let testPrimitiveTwo: NumVal = 2;
 let testPrimitiveThree: NumVal = 3;
 let testPrimitiveArray: NumVal = [1, 2, 3, [1, 2, 2], [1, 2, 3]];
+// The test below will not work because the array contains the value 4.
 let testPrimitiveArrayTwoFail: NumVal = [1, 2, 3, 4];
+
+// Take away: types can give a name to any primitive number, string, boolean, undefined and null. interfaces provide a way to define objects.
