eaf9be0d5316a7978642a540be9cf2f65412247b
Edwin Rivera <waehake@gmail.com>
2019-04-18 10:18:30 -0400

    added an example generic by which shows that generic types can be used in closure scope.

--- generalpractice/generics.ts
+++ generalpractice/generics.ts
@@ -1,4 +1,4 @@
-// Generics let you define interfaces and functions that accept a type that is "dynamic"; Think of Generic Type or passing a Dynamic Type;
+// Generics let you define interfaces and functions that accept a type that is "dynamic"; Think of Generic Type or passing a Dynamic Type; A convention when defining a generic is to use a single capital letter.
 
 interface HasPhone<T> {
   phone: T;
@@ -11,3 +11,25 @@ let phoneNumberAsString: HasPhone<string> = {
 let phoneNumberAsNumber: HasPhone<number> = {
   phone: 9735555604
 };
+
+// Generics can have a default type.
+interface CheckForString<T = any> {
+  (val: T): boolean;
+}
+// This function will check the type of the text argument is indeed a string.
+let stringCheck: CheckForString<string> = text => typeof text === "string";
+
+stringCheck("text");
+// type error because we passed a number
+stringCheck(2);
+
+// define a closure with generic types, generic types scope works just like variable scope.
+function outerTuple<T>(a: T) {
+  return function innerTuple<H>(b: H) {
+    return [a, b] as [T, H];
+  };
+}
+
+let newItemPair = outerTuple(["Cool", "text"])(3);
+
+console.log(newItemPair);
