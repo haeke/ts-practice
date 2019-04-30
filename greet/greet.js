a6ff17950261430991830c147876e23023ad3966
Edwin Rivera <waehake@gmail.com>
2019-04-30 10:43:36 -0400

    added functions to add and remove classes from the greeting file.

--- greet/greet.js
+++ greet/greet.js
@@ -24,16 +24,62 @@ greetingButton.addEventListener("click", sendGreeting);
 var greeting = document.getElementById("welcomeGreeting");
 greeting.innerHTML = greeter(user);
 var _loop_1 = function (i) {
+    // get a reference to the buttonGroup div
     buttonGroup = document.getElementById("buttonGroup");
     // create a button element
     btn = document.createElement("button");
     // add the text to the button
     btn.appendChild(document.createTextNode("Button " + i));
-    // attach an event listener to do this: document.addEventListener
+    // add an event listener when a user clicks on the button
     btn.addEventListener("click", function () { return console.log("clicked on button ", i); });
+    // append the button inside the buttonGroup DOM element
     buttonGroup.appendChild(btn);
 };
 var buttonGroup, btn;
 for (var i = 0; i < 5; i++) {
     _loop_1(i);
 }
+var lunch = {
+    sandwich: "Ham",
+    snack: "Potato Chip's",
+    drink: "Sprite",
+    guests: 0,
+    alcohol: false
+};
+var food = document.getElementById("foodArea");
+if (food) {
+    for (var key in lunch) {
+        // as a best practice, you should check if the object contains the key before trying to add it to the DOM.
+        if (lunch.hasOwnProperty(key)) {
+            // create an element of the type that we want to set.
+            if (key === "guests")
+                break;
+            var p = document.createElement("p");
+            // set the innerHTML to the string
+            p.innerHTML = lunch[key];
+            // append the Node to the foodArea div
+            food.appendChild(p);
+        }
+    }
+}
+// remove the text-muted class from the paragraph text
+function removeTextMuted() {
+    var paragraphText = document.querySelector(".text-muted");
+    if (paragraphText) {
+        paragraphText.classList.remove("text-muted");
+    }
+}
+function toggleTextMuted() {
+    var paragraphText = document.querySelector(".toggleText");
+    if (paragraphText) {
+        paragraphText.classList.toggle("text-muted");
+    }
+}
+// You can use className to get all of the classes on an element as a string, add a class or classes, or completely replace or remove all classes.
+function centerText() {
+    var paragraph = document.querySelector(".text-muted");
+    if (paragraph) {
+        paragraph.className += " text-center";
+    }
+}
+centerText();
--- greet/greet.ts
+++ greet/greet.ts
@@ -50,3 +50,61 @@ for (let i = 0; i < 5; i++) {
   // append the button inside the buttonGroup DOM element
   buttonGroup.appendChild(btn);
 }
+
+interface Lunch {
+  sandwich: string;
+  snack: string;
+  drink: string;
+  guests: number;
+  alcohol: boolean;
+}
+
+let lunch: Lunch = {
+  sandwich: "Ham",
+  snack: "Potato Chip's",
+  drink: "Sprite",
+  guests: 0,
+  alcohol: false
+};
+
+let food = document.getElementById("foodArea");
+if (food) {
+  for (let key in lunch) {
+    // as a best practice, you should check if the object contains the key before trying to add it to the DOM.
+    if (lunch.hasOwnProperty(key)) {
+      // create an element of the type that we want to set.
+      if (key === "guests") break;
+      let p = document.createElement("p");
+      // set the innerHTML to the string
+      p.innerHTML = lunch[key];
+      // append the Node to the foodArea div
+      food.appendChild(p);
+    }
+  }
+}
+
+// remove the text-muted class from the paragraph text
+function removeTextMuted(): void {
+  let paragraphText: Element = document.querySelector(".text-muted");
+  if (paragraphText) {
+    paragraphText.classList.remove("text-muted");
+  }
+}
+
+function toggleTextMuted(): void {
+  let paragraphText: Element = document.querySelector(".toggleText");
+
+  if (paragraphText) {
+    paragraphText.classList.toggle("text-muted");
+  }
+}
+
+// You can use className to get all of the classes on an element as a string, add a class or classes, or completely replace or remove all classes.
+function centerText(): void {
+  let paragraph = document.querySelector(".text-muted");
+  if (paragraph) {
+    paragraph.className += " text-center";
+  }
+}
+
+centerText();
